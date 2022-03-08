523d8e7e3b6d6aae036111b2de536cc82100d98a
Alexander Fischenich <alexf@airvonalexander.speedport.ip>
2022-03-08 17:36:54 +0100

    add Storybook

---
+++ components/Buttons/button.stories.js
@@ -0,0 +1,6 @@
+import "./button.css";
+export default {
+  title: "Components/button",
+};
+export const Default = () => `<button class="button">Click me</button>`;
+export const Clicked = () => `<button class="button button--clicked">Click me</button>`;
